7d80fd807d8c3a3dc91b0c6c68c7ab5b5db0eb13
Magdalena Osinska <osinska.magdalenamaria@gmail.com>
2022-04-29 11:57:14 +0200

    wersja początkowa

---
+++ src/com/company/Ksztalt.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface Ksztalt {
+     double obliczPowierzchnie();
+     double obliczObwod();
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Kolo kolo = new Kolo(4);
+        System.out.println(kolo.obliczPowierzchnie());
+
+        Kwadrat kwadrat = new Kwadrat(10);
+        System.out.println(kwadrat.obliczPowierzchnie());
+        System.out.println(kwadrat.obliczObwod());
+    }
+}
+
+class Kwadrat implements Ksztalt {
+    double dlugoscBoku;
+
+    Kwadrat (double dlugoscBoku) {
+        this.dlugoscBoku = dlugoscBoku;
+    }
+
+    @Override
+    public double obliczPowierzchnie() {
+        return dlugoscBoku * dlugoscBoku;
+    }
+
+    @Override
+    public double obliczObwod() {
+
+        return dlugoscBoku * 4;
+    }
+}
+class Kolo implements Ksztalt {
+    double promien;
+    Kolo(double promien) {
+
+        this.promien = promien;
+    }
+
+    @Override
+    public double obliczPowierzchnie() {
+
+        return Math.PI * promien * promien;
+    }
+
+    @Override
+    public double obliczObwod() {
+
+        return 2 * Math.PI * promien;
+    }
+}
